73d6d52639ec25c54cc3fc613db886ebddd6e6b9
michelle <michelle@25sprout.com>
2022-01-16 16:45:33 +0800

    feat: changed app wording

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hiii there!! Changed~
+          React App Started!
         </p>
         <a
           className="App-link"
